e33562f9ac38ea888a7a5b8a526273a5be382cdb
Jiucheng Zang <zangjiucheng@gmail.com>
2024-04-25 12:38:46 -0400

    Update

--- OnlineVersion/main.py
+++ OnlineVersion/main.py
@@ -23,32 +23,42 @@ if __name__ == '__main__':
     # checkDetail("CO 255")
 
     ###List of Course
-    dbClassUW.switchCollection(
-          collectionName="Class2023Fall")  # Class2023Fall Class2024Winter Class2024Spring Class2024Fall
-    addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 230",6872))
-    # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 231",6879))
-    # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 135"))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 145",6287))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 241",6398))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 237",6203))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 147",6507))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 135",6078))
-    # addCourse(SearchAvalibleInTerm(dbClassUW, "COMMST 100"))
-    # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 230"))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "MTHEL 131"))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "COMMST 100",7991))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 350",6583))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "CO 255",6522))
-    addCourse(SearchAvalibleInTerm(dbClassUW, "PHYS 468"))
-
     # dbClassUW.switchCollection(
-    #       collectionName="Class2024Winter")  # Class2023Fall Class2024Winter Class2024Spring Class2024Fall
+    #       collectionName="Class2023Fall")  # Class2023Fall Class2024Winter Class2024Spring Class2024Fall
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 230",6872))
+    # # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 231",6879))
+    # # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 135"))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 145",6287))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 241",6398))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 237",6203))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 147",6507))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 135",6078))
+    # # addCourse(SearchAvalibleInTerm(dbClassUW, "COMMST 100"))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 230"))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "MTHEL 131"))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "COMMST 100",7991))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 350",6583))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "CO 255",6522))
+    # addCourse(SearchAvalibleInTerm(dbClassUW, "PHYS 468"))
+
     # addCourse(SearchAvalibleInTerm(dbClassUW, "ECON 101"))
     # addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 230"))
     # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 146"))
     # addCourse(SearchAvalibleInTerm(dbClassUW, "CS 136L"))
     # addCourse(SearchAvalibleInTerm(dbClassUW, "ENGL 119"))
     # addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 136"))
+
+    
+    dbClassUW.switchCollection(
+          collectionName="Class2024Spring")  # Class2023Fall Class2024Winter Class2024Spring Class2024Fall
+
+    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 486", 3929)) # Introduction to Artificial Intelligence
+    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 246", 3726)) # Object-Oriented Software Development
+    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 245", 3795)) # Logic and Computation
+    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 349", 3810)) # User Interface Design
+    addCourse(SearchAvalibleInTerm(dbClassUW, "CS 350", 3926)) # Operating Systems
+    addCourse(SearchAvalibleInTerm(dbClassUW, "MATH 247", 3914)) # Calculus 3 (Advanced Level)
+    addCourse(SearchAvalibleInTerm(dbClassUW, "STAT 231", 3960)) # Introduction to Combinatorics
 
     ### List Course End
     makeSchedule(dbClassUW, courseWishList=courseWishList)
